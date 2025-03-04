a09583246293c155ef5d5f80981aaab35499d857
Somebud0180 <ethanlagera@icloud.com>
2025-03-04 14:50:27 +0800

    Update to Godot 4.4, and a new maze level ig

--- scripts/main.gd
+++ scripts/main.gd
@@ -4,7 +4,6 @@ extends NinePatchRect
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var platform = OS.get_name()
-	print(platform)
 	if platform == "Web" or platform == "iOS":
 		$VBoxContainer/ButtonSpacer4.visible = false
 		$VBoxContainer/Quit.visible = false
