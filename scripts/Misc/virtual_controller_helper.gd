197daf87fb25ac4df41abcef765e42760f6e1219
Somebud0180 <ethanlagera@icloud.com>
2025-03-19 21:21:53 +0800

    Update virtual_controller fix

--- scripts/Misc/virtual_controller_helper.gd
+++ scripts/Misc/virtual_controller_helper.gd
@@ -5,6 +5,6 @@ class_name VirtualControllerHelper
 # Define the method as static
 static func get_virtual_controller():
 	if Input.has_method("get_virtual_controller"):
-		return Input.get_virtual_controller()
+		return Input.call("get_virtual_controller")
 	else:
 		return null
